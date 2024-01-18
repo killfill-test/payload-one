cd29f666400326663c0e937bf5d11c5ffe3c82da
killfill-test <45947780+killfill-test@users.noreply.github.com>
2024-01-18 14:20:54 -0300

    Create Contents.ts

---
+++ src/collections/Contents.ts
@@ -0,0 +1,15 @@
+import { CollectionConfig } from 'payload/types'
+
+const Contents: CollectionConfig = {
+  slug: 'contents',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {name: 'title', type: 'text'},
+    {name: 'url', type: 'text'},
+    {name: 'estado', type: 'select', options: [{label: 'Dale', value: 'dale'}, {label: 'Listo', value: 'listo'}]
+  ],
+}
+
+export default Contents
